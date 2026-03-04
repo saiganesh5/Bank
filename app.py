c1209f30b507c0ef4c223e8d11d026e3019c2374
Sai penaganti <118742743+12219418@users.noreply.github.com>
2026-03-04 22:23:55 +0530

    Change server port to use environment variable
    Updated server port configuration to use environment variable.

--- app.py
+++ app.py
@@ -138,4 +138,6 @@ def predict_ui():
 # -------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
