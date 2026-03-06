4a0d46a0e86d8371cbca7240f0ec3e4f7c196121
Sai penaganti <118742743+12219418@users.noreply.github.com>
2026-03-06 22:50:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -o errexit
 
 apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-eng
+apt-get install -y tesseract-ocr tesseract-ocr-eng libgl1 libglib2.0-0
 
 pip install -r requirements.txt
